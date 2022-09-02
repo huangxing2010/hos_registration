38d6a83ea83809bcf9ea6da9217fc19cfd3147b0
huangxing <1398474457@qq.com>
2022-09-02 17:48:38 +0800

    first commit
    Signed-off-by: huangxing <1398474457@qq.com>

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -2,23 +2,8 @@
 
 return [
     'autoload' => false,
-<<<<<<< HEAD
-    'hooks' => [
-        'leesignhook' => [
-            'leesign',
-        ],
-    ],
-    'route' => [
-        '/example$' => 'example/index/index',
-        '/example/d/[:name]' => 'example/demo/index',
-        '/example/d1/[:name]' => 'example/demo/demo1',
-        '/example/d2/[:name]' => 'example/demo/demo2',
-        '/leesign$' => 'leesign/index/index',
-    ],
-=======
     'hooks' => [],
     'route' => [],
->>>>>>> fb984c07e8af4bbba37dd2e93cde82c5f3526516
     'priority' => [],
     'domain' => '',
 ];
